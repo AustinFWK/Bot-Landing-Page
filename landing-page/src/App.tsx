f3f1a5b3da5f9645e91a5277382e99b57a24378f
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2026-03-23 14:47:08 -0400

    add hero template

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -1,7 +1,10 @@
+import Hero from "./sections/hero/Hero";
+
 function App() {
   return (
     <div>
       <h1>Valorant Patch Notes Bot</h1>
+      <Hero />
     </div>
   );
 }
---
+++ landing-page/src/sections/hero/Hero.tsx
@@ -0,0 +1,20 @@
+export default function Hero() {
+  return (
+    <div>
+      <h2>Never miss a Valorant Patch again!</h2>
+      <span>
+        Get instant, clean, easy-to-read patch notes delivered straight to your
+        Discord server. <br />
+      </span>
+      <span>
+        install to discord{" "}
+        <a
+          href="https://discord.com/oauth2/authorize?client_id=1204815740918693898"
+          target="_blank"
+        >
+          here
+        </a>
+      </span>
+    </div>
+  );
+}
