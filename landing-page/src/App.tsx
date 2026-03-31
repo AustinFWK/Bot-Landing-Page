b3962b1f6ae41c7babc46009b4429ae9124be0ca
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2026-03-31 14:25:50 -0400

    update global css, install shadcn button

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -4,10 +4,7 @@ import { ThemeProvider } from "./components/themeProvider";
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div>
-        <h1>Valorant Patch Notes Bot</h1>
-        <Hero />
-      </div>
+      <Hero />
     </ThemeProvider>
   );
 }
--- landing-page/src/sections/hero/Hero.tsx
+++ landing-page/src/sections/hero/Hero.tsx
@@ -1,20 +1,16 @@
+import { Button } from "@/../components/ui/button";
+
 export default function Hero() {
   return (
-    <div>
-      <h2>Never miss a Valorant Patch again!</h2>
-      <span>
-        Get instant, clean, easy-to-read patch notes delivered straight to your
-        Discord server. <br />
-      </span>
-      <span>
-        install to discord{" "}
-        <a
-          href="https://discord.com/oauth2/authorize?client_id=1204815740918693898"
-          target="_blank"
-        >
-          here
-        </a>
-      </span>
-    </div>
+    <section className="section">
+      <div className="container">
+        <h2>Never miss a Valorant Patch again!</h2>
+        <span>
+          Get instant, clean, easy-to-read patch notes delivered straight to
+          your Discord server. <br />
+        </span>
+        <Button>Invite to server</Button>
+      </div>
+    </section>
   );
 }
