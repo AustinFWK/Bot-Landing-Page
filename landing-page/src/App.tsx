68d2e309be3be31f4928e499694bbc2b75fcf13c
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2026-03-31 20:40:23 -0400

    add problem solution section

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -1,10 +1,12 @@
 import Hero from "./sections/hero/Hero";
+import ProblemSolution from "./sections/problem/Problem";
 import { ThemeProvider } from "./components/themeProvider";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Hero />
+      <ProblemSolution />
     </ThemeProvider>
   );
 }
---
+++ landing-page/src/sections/problem/Problem.tsx
@@ -0,0 +1,13 @@
+export default function ProblemSolution() {
+  return (
+    <section className="section">
+      <div className="container">
+        <h3>Patch notes are long, cluttered, and easy to miss</h3>
+        <div>
+          With the Valorant Patch Bot, it sends the important stuff directly to
+          your server - formatted for quick reading.
+        </div>
+      </div>
+    </section>
+  );
+}
