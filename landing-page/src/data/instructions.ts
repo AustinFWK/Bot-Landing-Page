737496d278a866852b7fe7d73b58e773aec77a5f
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2026-04-15 14:36:01 -0400

    update reverse property in instruction type

--- landing-page/src/data/instructions.ts
+++ landing-page/src/data/instructions.ts
@@ -3,12 +3,25 @@ export type Instruction = {
   title: string;
   description: string;
   image: string;
-  reverse?: false;
+  reverse?: boolean;
 };
 
 export const instructions: Instruction[] = [
   {
-    step: "01",
+    step: "1",
+    title: "Invite the bot",
+    description: "Add the bot to your discord server in seconds.",
+    image: "",
+  },
+  {
+    step: "2",
+    title: "Choose a channel",
+    description: "Select which channel you would like patch notes posted in.",
+    image: "",
+    reverse: true,
+  },
+  {
+    step: "3",
     title: "Invite the bot",
     description: "Add the bot to your discord server in seconds.",
     image: "",
