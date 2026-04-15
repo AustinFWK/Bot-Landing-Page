52838860641c3ea3258ab1537a27e5fd2bf20c36
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2026-04-15 12:26:58 -0400

    rename problem -> how it works

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -1,5 +1,5 @@
 import Hero from "./sections/hero/Hero";
-import ProblemSolution from "./sections/problem/Problem";
+import HowItWorks from "./sections/functionality/howItWorks";
 import Features from "./sections/features/Features";
 import CallToAction from "./sections/cta/callToAction";
 import { ThemeProvider } from "./components/themeProvider";
@@ -8,7 +8,7 @@ function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Hero />
-      <ProblemSolution />
+      <HowItWorks />
       <Features />
       <CallToAction />
     </ThemeProvider>
--- landing-page/src/sections/functionality/howItWorks.tsx
+++ landing-page/src/sections/functionality/howItWorks.tsx
@@ -1,8 +1,8 @@
-export default function ProblemSolution() {
+export default function HowItWorks() {
   return (
     <section className="section">
       <div className="container">
-        <h3>Patch notes are long, cluttered, and easy to miss</h3>
+        <h2>Patch notes are long, cluttered, and easy to miss</h2>
         <div>
           With the Valorant Patch Bot, it sends the important stuff directly to
           your server - formatted for quick reading.
